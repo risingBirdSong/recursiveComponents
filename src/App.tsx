2190afb4a190add0a48e13d50c801f3c000466ee
risingbirdsong <heregoingthere@outlook.com>
2020-03-20 20:45:00 -0700

    a few really cool looking colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { findByLabelText } from '@testing-library/react';
 
 
 interface rcrsPlyA_propsI {
   content: string;
-  recursiveCount: number;
+  cnt: number;
 }
 
 const rcrsPlyA = (props: rcrsPlyA_propsI) => {
-  if (props.recursiveCount <= 0) {
+  if (props.cnt <= 0) {
     return props.content;
   }
-  props.recursiveCount--;
+  props.cnt--;
   return (
-    <div style={{ backgroundColor: "beige", border: "1px solid black", width: props.recursiveCount * 20, height: props.recursiveCount * 20, display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ backgroundColor: `rgb(${255 - props.cnt * 5}, 55, ${55 + props.cnt * 10})`, border: "1px solid black", width: props.cnt * 80, height: props.cnt * 80, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: `${props.cnt * 4}px`, boxShadow: `${props.cnt}px -${props.cnt}px 20px 1px black` }}>
       {rcrsPlyA(props)}
     </div>
   )
@@ -25,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <div className="myContainer">
-        {rcrsPlyA({ content: "!", recursiveCount: 10 })}
+        {rcrsPlyA({ content: "!", cnt: 10 })}
       </div>
     </div>
   );
