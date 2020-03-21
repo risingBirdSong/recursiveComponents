a61a5a559b00940a9b460b01ff862d432442d02a
risingbirdsong <heregoingthere@outlook.com>
2020-03-20 21:07:32 -0700

    added some more potential props to my interface, added a button toggler, some more styling particularly flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +6,9 @@ import './App.css';
 interface rcrsPlyA_propsI {
   content: string;
   cnt: number;
+  border?: {
+    thickness: number
+  }
 }
 
 const rcrsPlyA = (props: rcrsPlyA_propsI) => {
@@ -14,17 +17,24 @@ const rcrsPlyA = (props: rcrsPlyA_propsI) => {
   }
   props.cnt--;
   return (
-    <div style={{ backgroundColor: `rgb(${255 - props.cnt * 5}, 55, ${55 + props.cnt * 10})`, border: "1px solid black", width: props.cnt * 80, height: props.cnt * 80, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: `${props.cnt * 4}px`, boxShadow: `${props.cnt}px -${props.cnt}px 20px 1px black` }}>
+    <div style={{ backgroundColor: `rgb(${255 - props.cnt * 5}, 55, ${55 + props.cnt * 4})`, border: ` ${props.border?.thickness ?? 1}px solid black`, width: props.cnt * 80, height: props.cnt * 80, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: `${props.cnt * 5}px`, boxShadow: `${props.cnt}px -${props.cnt}px 20px 1px darkmagenta` }}>
       {rcrsPlyA(props)}
     </div>
   )
 }
 
 function App() {
+  let [toggleFirst, settoggleFirst] = useState(false)
   return (
     <div className="App">
+      <div className="buttonHolder">
+        <button onClick={() => {
+          settoggleFirst(!toggleFirst);
+        }}>1st</button>
+      </div>
       <div className="myContainer">
-        {rcrsPlyA({ content: "!", cnt: 10 })}
+
+        {toggleFirst ? rcrsPlyA({ content: "!", cnt: 10, border: { thickness: 5 } }) : ""}
       </div>
     </div>
   );
