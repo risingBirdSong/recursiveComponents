9cc996957e32a8cc10eba7268cbde00b1766b714
risingbirdsong <heregoingthere@outlook.com>
2020-03-20 20:13:22 -0700

    first recursive component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+interface rcrsPlyA_propsI {
+  content: string;
+  recursiveCount: number;
+}
+
+const rcrsPlyA = (props: rcrsPlyA_propsI) => {
+  if (props.recursiveCount <= 0) {
+    return props.content;
+  }
+  props.recursiveCount--;
+  return (
+    <div style={{ backgroundColor: "beige", border: "3px solid black", width: props.recursiveCount * 20, height: props.recursiveCount * 20 }}>
+      {rcrsPlyA(props)};
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="myContainer">
+        {rcrsPlyA({ content: "hello world", recursiveCount: 10 })}
+      </div>
     </div>
   );
 }
