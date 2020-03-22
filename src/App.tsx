8472d3b4509973c61017c666b1fa22577a6aa59f
risingbirdsong <heregoingthere@outlook.com>
2020-03-21 22:01:07 -0700

    purple flower and other css play

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 
 
@@ -9,10 +8,10 @@ interface rcrsPlyA_propsI {
   count: number;
   backgroundColorMultiply: number,
   shadowColor: string,
-  borderRadiusColorMult: number,
   borderColor: string,
   marginRight: number,
   marginTop: number,
+  inc: number
 }
 
 const rcrsPlyA = (props: rcrsPlyA_propsI) => {
@@ -20,25 +19,46 @@ const rcrsPlyA = (props: rcrsPlyA_propsI) => {
     return props.content;
   }
   props.count--;
+  props.inc += 5;
+  console.log("divide", 1 - (1 / (props.count)))
+  let randoMul = 40;
   return (
-    <div style={{ backgroundColor: `rgb(${255 - props.count * props.backgroundColorMultiply}, 20, ${55 + props.count * 1})`, border: `${props.count}px solid ${props.borderColor}`, width: props.count * 80, height: props.count * 80, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: `${props.count * props.borderRadiusColorMult}px`, boxShadow: `-10px -10px 5px -15px ${props.shadowColor}, ${props.count * -1.5 - 7}px ${props.count * -1.2 - 7}px 5px -10px black , inset ${props.count * 1.5}px ${props.count * 1.5}px ${props.count * 2}px ${props.count}px orange `, marginTop: `${props.marginTop * props.count}px`, marginRight: `${props.marginRight * props.count}px`, padding: props.count * 2 }}>
+    <div style={{
+      backgroundColor: `rgba(${255 - props.count * props.backgroundColorMultiply}, 20, ${55 + props.count * 1}, ${1 - 1 / (props.count)})`,
+      border: `1px inset ${props.borderColor}`,
+      width: props.count * 40,
+      height: props.count * 40,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      // / ${40 + props.inc}px ${10 + props.inc}px ${15 + props.inc}px ${30 + props.inc}px
+      borderRadius:
+        `
+        ${4 * props.inc + Math.floor(Math.random() * randoMul)}px ${2 * props.inc + Math.floor(Math.random() * randoMul)}px ${2 * props.inc + Math.floor(Math.random() * randoMul)}px ${4 * props.inc + Math.floor(Math.random() * randoMul)}px
+        /
+        ${5 * props.inc + Math.floor(Math.random() * randoMul)}px ${9 * props.inc + Math.floor(Math.random() * randoMul)}px ${16 * props.inc + Math.floor(Math.random() * randoMul)}px ${5 * props.inc + Math.floor(Math.random() * randoMul)}px
+          `,
+
+      boxShadow: `7px 7px 22px -3px purple, -10px -10px 15px -3px black , inset 3px 3px 10px ${props.inc / 2}px blueviolet`,
+      transform: `rotate(50deg)`
+    }}>
       {rcrsPlyA(props)}
     </div>
   )
 }
 
 function App() {
-  let [toggleFirst, settoggleFirst] = useState(false)
+  let [toggleFirst, settoggleFirst] = useState(true)
   return (
     <div className="App">
-      <div className="buttonHolder">
+      {/* <div className="buttonHolder">
         <button onClick={() => {
           settoggleFirst(!toggleFirst);
         }}>1st</button>
-      </div>
+      </div> */}
       <div className="myContainer">
 
-        {toggleFirst ? rcrsPlyA({ content: ".", count: 10, backgroundColorMultiply: 15, shadowColor: "red", borderRadiusColorMult: 15, borderColor: "coral", marginTop: 7, marginRight: 7 }) : ""}
+        {toggleFirst ? rcrsPlyA({ content: ".", count: 15, backgroundColorMultiply: 15, shadowColor: "red", borderColor: "red", inc: 0, marginTop: 7, marginRight: 7 }) : ""}
       </div>
     </div>
   );
